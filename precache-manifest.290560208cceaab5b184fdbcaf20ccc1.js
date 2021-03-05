b029280d17fd3157a0edff976279dcca5bce0b75
Alexei <demidenko8888@gmail.com>
2021-03-05 10:57:06 +0300

    Updates

---
+++ precache-manifest.290560208cceaab5b184fdbcaf20ccc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac83cd51d219fbd5a163fe9eaecf692c",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "5a7bc4ea82a0132313fe",
+    "url": "/test-app/static/css/main.b2a7682b.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/test-app/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-app/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7bc4ea82a0132313fe",
+    "url": "/test-app/static/js/main.6952cecc.chunk.js"
+  },
+  {
+    "revision": "1c995138ec350d72049f",
+    "url": "/test-app/static/js/runtime-main.1c39affa.js"
+  }
+]);
